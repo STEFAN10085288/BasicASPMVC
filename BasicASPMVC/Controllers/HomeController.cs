5ac3460f98b12aa24b63a3c85696d53b25086930
Stefan <st10085288@vcconnect.edu.za>
2023-06-07 08:37:30 +0200

    Add project files.

---
+++ BasicASPMVC/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using BasicASPMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BasicASPMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Stefan";
+            ViewBag.Date = DateTime.Now;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BasicASPMVC/Controllers/StaffController.cs
@@ -0,0 +1,86 @@
+﻿using BasicASPMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Elfie.Model;
+
+namespace BasicASPMVC.Controllers
+{
+    public class StaffController : Controller
+    {
+        StaffContext db = new StaffContext();
+        public IActionResult Index()
+        {
+            return View(StaffContext.staffObject);
+        }
+
+
+        public IActionResult AddStaff()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddStaff(Staff sf)
+        {
+            Staff data = sf;
+            if (sf.Name == null || sf.Id < 0 || sf.Password == null || sf.Dob == null || sf.Title == null)
+            {
+                ViewBag.Error = "Please enter all fields";
+                return View();
+            }
+            else
+            {
+                StaffContext.staffObject.Add(data);
+                return RedirectToAction("Index");
+            }
+        }
+
+
+        public IActionResult EditStaff(int id)
+        {
+            Staff temp = StaffContext.staffObject.Where(s => s.Id == id).FirstOrDefault();
+            return View(temp);
+        }
+
+        [HttpPost]
+        public IActionResult EditStaff(Staff sf) 
+        {
+            Staff data = sf;
+            if (sf.Name == null || sf.Id < 0 || sf.Password == null || sf.Dob == null || sf.Title == null)
+            {
+                ViewBag.Error = "Please enter all fields";
+                return View();
+            }
+            else
+            {
+                (from p in StaffContext.staffObject where p.Id == sf.Id select p).ToList().ForEach(x =>
+                {
+                    x.Password = sf.Password;
+                    x.Dob = sf.Dob;
+                    x.Title = sf.Title;
+                    x.Name = sf.Name;
+
+                });
+                return RedirectToAction("Index");
+            }         
+
+        }
+        public IActionResult ViewStaff(int id)
+        {
+            Staff temp = StaffContext.staffObject.Where(s => s.Id == id).FirstOrDefault();
+            return View(temp);
+        }
+
+        public IActionResult DeleteStaff(int id)
+        {
+            Staff temp = StaffContext.staffObject.Where(s => s.Id == id).FirstOrDefault();
+            return View(temp);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteStaff(Staff sf)
+        {
+            StaffContext.staffObject = StaffContext.staffObject.Where(s => s.Id != sf.Id).ToList();
+            return RedirectToAction("Index");
+        }
+    }
+}
